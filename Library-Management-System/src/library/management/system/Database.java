55d1635bef56ea5f4eae8ae4fec12021ee3cfb14
ahmed151515 <ahmedarafa.200444@gmail.com>
2023-12-13 15:43:20 +0200

    add Comments in Database class

--- Library-Management-System/src/library/management/system/Database.java
+++ Library-Management-System/src/library/management/system/Database.java
@@ -5,9 +5,11 @@ import java.io.IOException;
 import java.sql.*;
 
 /**
- * Database - to handel database part
+ * Database class handles the interaction with the database for the Library Management System.
+ * It provides methods for executing SQL queries and updates related to users, books, and borrowing records.
  */
 public class Database {
+	// Database connection details
 	static String url = "jdbc:sqlserver://"
 			+ "192.168.1.12:1433;" // server name or IP and port
 			+ "Database=Library_Management_System;" // database name
@@ -16,8 +18,19 @@ public class Database {
 			+ "trustServerCertificate=true";
 	static String user = "admin";
 	static String password = "admin";
+
+	// Database connection object
 	private static Connection con;
 
+
+	// PreparedStatement and SQL statement for queries
+	private static PreparedStatement stmt;
+	private static String sql;
+
+	/**
+	 * Static block to establish a connection to the database when the class is loaded.
+	 * Throws a RuntimeException if a connection cannot be established.
+	 */
 	static {
 		try {
 			con = DriverManager.getConnection(url, user, password);
@@ -26,11 +39,6 @@ public class Database {
 		}
 	}
 
-	private static PreparedStatement stmt;
-	private static String sql;
-
-
-
 	public Connection getCon() {
 		return con;
 	}
@@ -38,7 +46,6 @@ public class Database {
 	public PreparedStatement getStmt() {
 		return stmt;
 	}
-
 
 
 	// close Connection
@@ -74,324 +81,438 @@ public class Database {
 //		return count;
 //	}
 //
-	// count books to set count in book
-	public  static int count_books() throws SQLException {
 
-		int count = 0;
-		sql = "select COUNT(book_ID) from books;";
+	// This function counts the number of books in the database.
+	public static int count_books() throws SQLException {
+		int count = 0; // Variable to store the number of books.
+
+		// SQL query to count the rows in the 'books' table.
+		String sql = "SELECT COUNT(book_ID) FROM books;";
+
 		try {
-			stmt = con.prepareStatement(sql);
+			// Prepare and execute the query using PreparedStatement.
+			PreparedStatement stmt = con.prepareStatement(sql);
 			ResultSet rs = stmt.executeQuery();
+
+			// Check the results and extract the count.
 			if (rs.next()) {
 				count = rs.getInt(1);
 			}
-			stmt.close();
 
+			// Close the statement to free up resources.
+			stmt.close();
 		} catch (Exception e) {
+			// Handle any exceptions and close the statement.
 			stmt.close();
 			System.out.println(e);
 		}
+
+		// Return the total count of books.
 		return count;
 	}
 
-	// handle select Statement
+
+	// This function executes a SELECT statement on the database and returns the ResultSet.
 	public static ResultSet select_stmt(String columns, String tables) throws SQLException {
-		sql = "select " + columns + " from " + tables + ";";
+		// Construct the SQL query by concatenating columns and tables.
+		String sql = "SELECT " + columns + " FROM " + tables + ";";
 
 		try {
+			// Prepare the statement and execute the query.
 			stmt = con.prepareStatement(sql);
-
 			return stmt.executeQuery();
 		} catch (Exception e) {
+			// Handle any exceptions, close the statement, and print the error message.
 			close_stmt();
 			System.out.println(e);
 		}
+
+		// Return null in case of an exception or no result.
 		return null;
 	}
 
-	// handle select Statement and where
+
+	// This function executes a parameterized SELECT statement on the database and returns the ResultSet.
 	public static ResultSet select_stmt(String columns, String tables, String conditions) throws SQLException {
-		sql = "select " + columns + " from " + tables + " where " + conditions + ";";
-		try {
-			stmt = con.prepareStatement(sql);
+		// Construct the SQL query by concatenating columns, tables, and conditions.
+		String sql = "SELECT " + columns + " FROM " + tables + " WHERE " + conditions + ";";
 
-			return  stmt.executeQuery();
+		try {
+			// Prepare the statement and execute the query.
+			stmt = con.prepareStatement(sql);
+			return stmt.executeQuery();
 		} catch (Exception e) {
+			// Handle any exceptions, close the statement, and print the error message.
 			close_stmt();
 			System.out.println(e);
 		}
+
+		// Return null in case of an exception or no result.
 		return null;
 	}
 
-	// handle insert Statement to user
+
+	// This function inserts a new user into the 'users' table.
 	public static int insert_user(User user) throws SQLException {
-		sql = "insert into users values (?,?);";
-		int result = 0;
+		// SQL query to insert values into the 'users' table.
+		String sql = "INSERT INTO users VALUES (?, ?);";
+
+		int result = 0; // Variable to store the execution result.
+
 		try {
-
-
+			// Prepare the statement for execution.
 			stmt = con.prepareStatement(sql);
 
-
+			// Set the values for the placeholders in the prepared statement.
 			stmt.setString(1, user.getPassword());
 			stmt.setString(2, user.getEmail());
+
+			// Execute the update query and store the result.
 			result = stmt.executeUpdate();
 		} catch (Exception e) {
+			// Handle any exceptions, close the statement, and print the error message.
 			stmt.close();
 			System.out.println(e);
 		}
 
+		// Return the result of the execution.
 		return result;
 	}
 
+
+	// This function updates user information in the 'users' table based on the user ID.
 	public static int update_user(User user) throws SQLException {
-		sql = "update users set password=?, email=? where user_id = ?;";
-		int result = 0;
+		// SQL query to update user information in the 'users' table.
+		String sql = "UPDATE users SET password=?, email=? WHERE user_id = ?;";
+
+		int result = 0; // Variable to store the execution result.
+
 		try {
+			// Prepare the statement for execution.
 			stmt = con.prepareStatement(sql);
 
-			stmt.setInt(3, user.getUser_ID());
+			// Set the values for the placeholders in the prepared statement.
 			stmt.setString(1, user.getPassword());
 			stmt.setString(2, user.getEmail());
+			stmt.setInt(3, user.getUser_ID());
+
+			// Execute the update query and store the result.
 			result = stmt.executeUpdate();
 		} catch (Exception e) {
+			// Handle any exceptions, close the statement, and print the error message.
 			stmt.close();
 			System.out.println(e);
 		}
 
+		// Return the result of the execution.
 		return result;
 	}
 
+
+	// This function deletes a user from the 'users' table based on the user ID.
 	public static int delete_user(User user) throws SQLException {
-		sql = "delete from users where user_ID=?;";
-		int result = 0;
+		// SQL query to delete a user from the 'users' table.
+		String sql = "DELETE FROM users WHERE user_ID=?;";
+
+		int result = 0; // Variable to store the execution result.
+
 		try {
-
-
+			// Prepare the statement for execution.
 			stmt = con.prepareStatement(sql);
 
+			// Set the value for the placeholder in the prepared statement.
 			stmt.setInt(1, user.getUser_ID());
 
+			// Execute the delete query and store the result.
 			result = stmt.executeUpdate();
-
 		} catch (Exception e) {
+			// Handle any exceptions, close the statement, and print the error message.
 			stmt.close();
 			System.out.println(e);
 		}
 
+		// Return the result of the execution.
 		return result;
 	}
 
+
+	// This function retrieves book categories based on the provided book ID.
 	public static String getBookCategories(int bookId) throws SQLException {
-		// Assuming you have a method to retrieve categories for a book based on book_ID
-		ResultSet resultSet = select_stmt("category", "book_categorise", "book_ID = " + bookId);
+		// Execute a SELECT statement to retrieve categories for the specified book ID.
+		ResultSet resultSet = select_stmt("category", "book_categories", "book_ID = " + bookId);
+
+		// StringBuilder to store the retrieved categories.
 		StringBuilder categories = new StringBuilder();
+
+		// Iterate through the result set and append categories to the StringBuilder.
 		while (resultSet.next()) {
 			categories.append(resultSet.getString("category")).append(", ");
 		}
+
+		// Remove the trailing comma and space if categories are present.
 		if (categories.length() > 2) {
-			categories.setLength(categories.length() - 2); // Remove the trailing comma and space
+			categories.setLength(categories.length() - 2);
 		}
+
+		// Return the formatted categories as a string.
 		return categories.toString();
 	}
 
-	// handle insert Statement to book
+
+	// This function inserts a new book into the 'books' table along with its categories.
+	// It requires an Admin object for authorization and a Book object for book details.
 	public static int insert_book(Admin admin, Book book) throws SQLException, IOException {
-
-
-		sql = "insert into books values (?,?,?,?,?,?,?);";
+		// SQL query to insert values into the 'books' table.
+		String sql = "INSERT INTO books VALUES (?,?,?,?,?,?,?);";
 		int result = 0;
 
 		try {
+			// Prepare the statement for executing the first insert query.
 			stmt = con.prepareStatement(sql);
 
-			stmt.setInt(1,book.getBook_id());
+			// Set the values for the placeholders in the prepared statement.
+			stmt.setInt(1, book.getBook_id());
 			stmt.setString(2, book.getName());
 			stmt.setInt(3, book.getNum_page());
 			stmt.setString(4, book.getDescribtion());
 			stmt.setString(5, book.getAuthor_name());
 			stmt.setBoolean(6, book.isTranslator());
-
 			stmt.setInt(7, admin.getUser_ID());
 
+			// Execute the first insert query and store the result.
 			result = stmt.executeUpdate();
 		} catch (SQLException e) {
+			// Handle SQL exceptions, close the statement, and print the error.
 			stmt.close();
 			e.printStackTrace();
-
 		}
-		for (String category : book.getCategory()) {
 
-			sql = "insert into book_categorise values (?,?);";
+		// Insert book categories into the 'book_categories' table.
+		for (String category : book.getCategory()) {
+			sql = "INSERT INTO book_categorise VALUES (?,?);";
 			try {
+				// Prepare the statement for executing the second insert query.
 				stmt = con.prepareStatement(sql);
 
+				// Set the values for the placeholders in the prepared statement.
 				stmt.setInt(1, book.getBook_id());
 				stmt.setString(2, category);
 
+				// Execute the second insert query.
 				stmt.executeUpdate();
-				stmt.close();
 
+				// Close the statement.
+				stmt.close();
 			} catch (Exception e) {
+				// Handle exceptions, close the statement, and print the error.
 				stmt.close();
 				System.out.println(e);
 			}
 		}
 
+		// Return the result of the first insert query.
 		return result;
 	}
 
-	public static int update_book(Admin admin, Book book) throws SQLException, IOException {
-		for (String category : book.getCategory()) {
 
-			sql = "update book_categorise set category=? where book_ID=?;";
+	// This function updates a book's information in the 'books' table and its categories in the 'book_categorise' table.
+// It requires an Admin object for authorization and a Book object for book details.
+	public static int update_book(Admin admin, Book book) throws SQLException, IOException {
+		// Update book categories in the 'book_categorise' table.
+		for (String category : book.getCategory()) {
+			String sql = "UPDATE book_categorise SET category=? WHERE book_ID=?;";
 			try {
+				// Prepare the statement for executing the update query.
 				stmt = con.prepareStatement(sql);
 
-				stmt.setInt(2, book.getBook_id());
+				// Set the values for the placeholders in the prepared statement.
 				stmt.setString(1, category);
+				stmt.setInt(2, book.getBook_id());
 
+				// Execute the update query.
 				stmt.executeUpdate();
+
+				// Close the statement.
 				stmt.close();
 			} catch (Exception e) {
+				// Handle exceptions, close the statement, and print the error.
 				stmt.close();
 				System.out.println(e);
 			}
 		}
 
-		sql = "update books set" + " book_name=?," // 1
-				+ " num_page=?,"  // 2
+		// Update book information in the 'books' table.
+		String sql = "UPDATE books SET"
+				+ " book_name=?,"       // 1
+				+ " num_page=?,"        // 2
 				+ " book_descnbtion=?," // 3
-				+ " book_author=?,"  // 4
-				+ " is_translator=?,"  // 5
-
-				+ " admin_ID=?"  // 7
-				+ " where book_ID=?;"; // 8
+				+ " book_author=?,"     // 4
+				+ " is_translator=?,"    // 5
+				+ " admin_ID=?"          // 6
+				+ " WHERE book_ID=?;";   // 7
 		int result = 0;
 
 		try {
+			// Prepare the statement for executing the update query.
 			stmt = con.prepareStatement(sql);
 
-			stmt.setInt(8, book.getBook_id());
+			// Set the values for the placeholders in the prepared statement.
 			stmt.setString(1, book.getName());
 			stmt.setInt(2, book.getNum_page());
 			stmt.setString(3, book.getDescribtion());
 			stmt.setString(4, book.getAuthor_name());
 			stmt.setBoolean(5, book.isTranslator());
-
 			stmt.setInt(6, admin.getUser_ID());
+			stmt.setInt(7, book.getBook_id());
 
+			// Execute the update query and store the result.
 			result = stmt.executeUpdate();
-
 		} catch (Exception e) {
+			// Handle exceptions, close the statement, and print the error.
 			stmt.close();
 			System.out.println(e);
 		}
 
-
+		// Return the result of the update query.
 		return result;
 	}
 
-	public static int delete_book(Admin admin, Book book) throws SQLException, IOException {
 
-
-		sql = "delete from book_categorise where book_ID=?;";
+	// This function deletes a book from the 'books' table along with its categories from the 'book_categorise' table.
+// It is assumed that the book deletion does not require admin authorization.
+	public static int delete_book(Book book) throws SQLException, IOException {
+		// Delete book categories from the 'book_categorise' table.
+		String sql = "DELETE FROM book_categorise WHERE book_ID=?;";
 		try {
+			// Prepare the statement for executing the delete query.
 			stmt = con.prepareStatement(sql);
 
-			stmt.setInt(2, book.getBook_id());
+			// Set the value for the placeholder in the prepared statement.
+			stmt.setInt(1, book.getBook_id());
 
+			// Execute the delete query.
 			stmt.executeUpdate();
 		} catch (Exception e) {
+			// Handle exceptions, close the statement, and print the error.
 			stmt.close();
 			System.out.println(e);
 		}
 
-
-		sql = "delete from books where book_ID=?;";
+		// Delete the book from the 'books' table.
+		sql = "DELETE FROM books WHERE book_ID=?;";
 		int result = 0;
 		try {
+			// Prepare the statement for executing the delete query.
 			stmt = con.prepareStatement(sql);
 
+			// Set the value for the placeholder in the prepared statement.
 			stmt.setInt(1, book.getBook_id());
-			stmt.executeUpdate();
 
+			// Execute the delete query and store the result.
 			result = stmt.executeUpdate();
 		} catch (Exception e) {
+			// Handle exceptions, close the statement, and print the error.
 			stmt.close();
 			System.out.println(e);
 		}
+
+		// Return the result of the delete query.
 		return result;
 	}
+
 
 	// handle insert Statement to borrow
+	// This function inserts a record of a user borrowing a book into the 'user_borrow_books' table.
 	public static int insert_borrow(Borrow borrow) throws SQLException {
-
-		java.sql.Date date;
-		sql = "insert into user_borrow_books values (?,?,?,?,?);";
+		// SQL query to insert values into the 'user_borrow_books' table.
+		sql = "INSERT INTO user_borrow_books VALUES (?,?,?,?,?);";
 		int result = 0;
+
 		try {
+			// Prepare the statement for executing the insert query.
 			stmt = con.prepareStatement(sql);
 
+			// Set the values for the placeholders in the prepared statement.
 			stmt.setInt(1, borrow.getUser_id());
 			stmt.setInt(2, borrow.getAdmin_id());
-			date = java.sql.Date.valueOf(borrow.getStart_date());
+			java.sql.Date date = java.sql.Date.valueOf(borrow.getStart_date());
 			stmt.setDate(3, date);
 			date = java.sql.Date.valueOf(borrow.getEnd_date());
 			stmt.setDate(4, date);
 			stmt.setInt(5, borrow.getBook_id());
 
+			// Execute the insert query and store the result.
 			result = stmt.executeUpdate();
 		} catch (Exception e) {
+			// Handle exceptions, close the statement, and print the error.
 			stmt.close();
 			System.out.println(e);
 		}
+
+		// Return the result of the insert query.
 		return result;
 	}
 
-	public static int update_borrow(Borrow borrow) throws SQLException {
 
-		java.sql.Date date;
-		sql = "update user_borrow_books set "
+	// This function updates a record of a user borrowing a book in the 'user_borrow_books' table.
+	public static int update_borrow(Borrow borrow) throws SQLException {
+		// SQL query to update values in the 'user_borrow_books' table.
+		sql = "UPDATE user_borrow_books SET "
 				+ "user_ID=?,"
 				+ " admin_ID=?,"
-				+ " strart_date=?,"
+				+ " start_date=?,"
 				+ " end_date=?"
-				+ "where book_id=?;";
+				+ " WHERE book_id=?;";
 		int result = 0;
+
 		try {
+			// Prepare the statement for executing the update query.
 			stmt = con.prepareStatement(sql);
 
+			// Set the values for the placeholders in the prepared statement.
 			stmt.setInt(1, borrow.getUser_id());
 			stmt.setInt(2, borrow.getAdmin_id());
-			date = java.sql.Date.valueOf(borrow.getStart_date());
+			java.sql.Date date = java.sql.Date.valueOf(borrow.getStart_date());
 			stmt.setDate(3, date);
 			date = java.sql.Date.valueOf(borrow.getEnd_date());
 			stmt.setDate(4, date);
 			stmt.setInt(5, borrow.getBook_id());
+
+			// Execute the update query and store the result.
 			result = stmt.executeUpdate();
 		} catch (Exception e) {
+			// Handle exceptions, close the statement, and print the error.
 			stmt.close();
 			System.out.println(e);
 		}
+
+		// Return the result of the update query.
 		return result;
 	}
 
-	public static int delete_borrow(Borrow borrow) throws SQLException {
 
-		java.sql.Date date;
-		sql = "delete from user_borrow_books where book_id=?";
+	// This function deletes a record of a user borrowing a book from the 'user_borrow_books' table.
+	public static int delete_borrow(Borrow borrow) throws SQLException {
+		// SQL query to delete a record from the 'user_borrow_books' table.
+		sql = "DELETE FROM user_borrow_books WHERE book_id=?";
 		int result = 0;
+
 		try {
+			// Prepare the statement for executing the delete query.
 			stmt = con.prepareStatement(sql);
 
+			// Set the value for the placeholder in the prepared statement.
 			stmt.setInt(1, borrow.getBook_id());
+
+			// Execute the delete query and store the result.
 			result = stmt.executeUpdate();
 		} catch (Exception e) {
+			// Handle exceptions, close the statement, and print the error.
 			stmt.close();
 			System.out.println(e);
 		}
 
+		// Return the result of the delete query.
 		return result;
 	}
 
