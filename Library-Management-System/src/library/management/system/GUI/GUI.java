d29c4efb8454461286d07aed99c0337860ed40cd
ahmed151515 <ahmedarafa.200444@gmail.com>
2023-12-16 20:22:50 +0200

    .

--- Library-Management-System/src/library/management/system/GUI/GUI.java
+++ Library-Management-System/src/library/management/system/GUI/GUI.java
@@ -5,26 +5,26 @@ import java.awt.*;
 import java.sql.SQLException;
 
 public class GUI {
-	static JFrame frame = new JFrame("Library Management System");
-	static CardLayout cardLayout;
-	static JPanel cardPanel;
+    static JFrame frame = new JFrame("Library Management System");
+    static CardLayout cardLayout;
+    static JPanel cardPanel;
 
-	public GUI() throws SQLException {
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public GUI() throws SQLException {
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-		NLogin loginPanel = new NLogin();
-		User_gui userGui = new User_gui();
-		Admin_gui adminGui = new Admin_gui();
-		cardLayout = new CardLayout();
-		cardPanel = new JPanel(cardLayout);
+        NLogin loginPanel = new NLogin();
+        User_gui userGui = new User_gui();
+        Admin_gui adminGui = new Admin_gui();
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel(cardLayout);
 
-		cardPanel.add(loginPanel, "LoginPanel");
-		cardPanel.add(userGui, "userGui");
-		cardPanel.add(adminGui, "adminGui");
-		frame.add(cardPanel);
-		cardLayout.show(cardPanel, "LoginPanel");
-		frame.pack();
-		frame.setLocationRelativeTo(null);
-		frame.setVisible(true);
-	}
+        cardPanel.add(loginPanel, "LoginPanel");
+        cardPanel.add(userGui, "userGui");
+        // cardPanel.add(adminGui, "adminGui");
+        frame.add(cardPanel); 
+        cardLayout.show(cardPanel, "LoginPanel");
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+    }
 }
